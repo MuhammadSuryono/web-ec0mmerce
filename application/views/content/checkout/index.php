b882d53d8821ba957b8912671cfd6c4b5ff818f3
Muhammad <yono@dmk.co.id>
2021-02-09 17:29:53 +0700

    Add category on checkout page

--- application/views/content/checkout/index.php
+++ application/views/content/checkout/index.php
@@ -8,17 +8,12 @@
                         <span>All Categories</span>
                     </div>
                     <ul>
-                        <li><a href="#">Fresh Meat</a></li>
-                        <li><a href="#">Vegetables</a></li>
-                        <li><a href="#">Fruit & Nut Gifts</a></li>
-                        <li><a href="#">Fresh Berries</a></li>
-                        <li><a href="#">Ocean Foods</a></li>
-                        <li><a href="#">Butter & Eggs</a></li>
-                        <li><a href="#">Fastfood</a></li>
-                        <li><a href="#">Fresh Onion</a></li>
-                        <li><a href="#">Papayaya & Crisps</a></li>
-                        <li><a href="#">Oatmeal</a></li>
-                        <li><a href="#">Fresh Bananas</a></li>
+                        <?php
+                        foreach ($category as $categor) { ?>
+                            <li><a href="<?= base_url('category/' . $categor->id) ?>"><?= $categor->category_name ?></a>
+                            </li>
+                        <?php }
+                        ?>
                     </ul>
                 </div>
             </div>
